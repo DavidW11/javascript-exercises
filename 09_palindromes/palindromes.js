44fc6e5752e9efdb80754a96acefcb6330bf7847
David Wang <davidwang7725@gmail.com>
2024-08-29 00:17:44 -0400

    Reviewed exercises 08-12 and completed exercises 09 and 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    // should make string lower case from the start
+    // can also you array reversal method shown in solution 
+    
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    let left = 0;
+    let right = word.length-1;
+
+    while (left<right) {
+        while (!alphanumerical.includes(word[left].toLowerCase())) {
+            left++;
+        }
+        while (!alphanumerical.includes(word[right].toLowerCase())) {
+            right--;
+        }
+        if (word[left].toLowerCase()!==word[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
